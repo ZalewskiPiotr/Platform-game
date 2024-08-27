b77e4a6aa201f96a49816587e8ff0a1e0895bf10
ZalewskiPiotr <1piotrzalewski@gmail.com>
2024-08-27 21:33:36 +0200

    Dodano stronę startową gry
    Utworzone menu główne. Na razie jest to pierwsza, mocno ograniczona wersja

---
+++ scenes/start_page.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+func _on_play_button_pressed() -> void:
+	print("Jestem w play")
+	pass # Replace with function body.
+
+
+func _on_help_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.
