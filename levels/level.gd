86f9a2851c244937ef49c024b4da9fa15dad6391
ZalewskiPiotr <1piotrzalewski@gmail.com>
2024-08-29 21:05:25 +0200

    Dziedziczenie sceny 'Level'
    Dodano główną scenę 'Level' z podstawową funkcjonalnością każdego układu. Kolejne układy będą dziedziczyć po klasie 'Level'. Zasady tworzenia dziedziczenia opisano w kalsie 'Level'

--- levels/level.gd
+++ levels/level.gd
@@ -2,7 +2,16 @@ extends Node
 class_name Level
 ## Skrypt, który zarządza tym co się dzieje w scenie levelu
 ##
-## Dodatkowy opis :)
+## Jest to główna klasa 'Level'. Wszystkie inne układy dziedziczą po tej klasie. Aby utworzyć
+## dziedziczoną scenę 'Level_x' należy kliknąć PPM na scenie 'Level' i wybrać opcję 'New Inherited
+## scene'. Utworzy się wówczas nowa scena z oznaczonymi na żółto podobiektami. Do takiej sceny 
+## będzie podłączony skrypt sceny głównej.
+## Jeżeli potrzebujemy przeciążyć metody klasy dziedziczonej lub dodać nowe to należy utworzyć nową 
+## klasę dziedziczoną jak powyżej. Następnie należy odłączyć skrypt i dodać nowy. W skrypcie należy 
+## dodać następujące linie:
+## extends Level
+## class_name LevelTest2
+## i napisać przeciążone lub nowe metody
 
 #region Stałe i zmienne
 @onready var _hud: CanvasLayer = $HUD	# Dostęp do HUD
--- levels/level_test_1.gd
+++ levels/level_test_1.gd
@@ -1,4 +1,5 @@
 extends Level
+class_name LevelTest1
 
 func _on_coin_collected() -> void:
 	super._on_coin_collected()
