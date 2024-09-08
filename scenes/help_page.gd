1e7b4e7d91093c32f1623416d3a4da63c40fcdb2
ZalewskiPiotr <1piotrzalewski@gmail.com>
2024-09-08 21:04:18 +0200

    Dodano stronę HELP
    Przy okazji dodania nowej strony nastąpiły zmiany w enumeratorze stron. W związku z tym konieczne były poprawki w obiekcie 'drzwi' w każdym układzie, ponieważ drzwi wskazywały na nieprawidłowy cel

---
+++ scenes/help_page.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _input(event: InputEvent) -> void:
+	# Wyjście do menu głównego
+	if event.is_action_pressed("ESC"):
+		_go_to_main_menu()
+
+func _on_button_pressed() -> void:
+	_go_to_main_menu()
+
+func _go_to_main_menu() -> void:
+	GameManager.reset_score()	# Trzeba wyzerować punkty
+	GameManager.goto_scene(GameManager.EnumScenes.START_PAGE)
